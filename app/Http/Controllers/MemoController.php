75fd6a1f077f601b285e08f5f410b01a03dc9dda
門脇大知 <baseballtaichi511218@gmail.com>
2021-02-02 23:57:29 +0900

    メモコントローラー作成

---
+++ app/Http/Controllers/MemoController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MemoController extends Controller
+{
+    //
+    /**
+     * 初期表示
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+        return view('memo');
+    }
+}
